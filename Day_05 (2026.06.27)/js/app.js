15a20e3530834e7d94243c2b8557527122c22883
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-06-27 15:07:02 +0530

    do while

--- Day_05 (2026.06.27)/js/app.js
+++ Day_05 (2026.06.27)/js/app.js
@@ -67,8 +67,16 @@
 // }
 
 // while loop
- let i = 0;
- while (i < 5) {
-     console.log(i);
-     i++;
- }
+//  let i = 0;
+//  while (i < 5) {
+//      console.log(i);
+//      i++;
+//  }
+
+// do while loop
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 5);            
+
