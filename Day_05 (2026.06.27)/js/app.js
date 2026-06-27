b9ff8e597e246ebdfddc342f7a8e0051dde556d3
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-06-27 14:39:20 +0530

    js

--- Day_05 (2026.06.27)/js/app.js
+++ Day_05 (2026.06.27)/js/app.js
@@ -1,8 +1,15 @@
 let name = "Githmi Thathsarani";
-
 let age = 20;
+let isAfterAL = "false";
+let customers = [];
+let customerList = {};
 
 console.log(name);
 console.log(age);
+
+let x = 10;
+let y = "10";
+
+console.log(x == y);
 
 alert("Hello, " + name + "! You are " + age + " years old.");
