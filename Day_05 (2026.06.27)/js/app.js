4c4c585ef3c31aa300a5fa962c6ca50659b747db
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-06-27 15:03:30 +0530

    for loop

--- Day_05 (2026.06.27)/js/app.js
+++ Day_05 (2026.06.27)/js/app.js
@@ -1,60 +1,67 @@
-let name = "Saman";
-let age = 18;
+// let name = "Saman";
+// let age = 18;
 
-name = "Ranil";
+// name = "Ranil";
 
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
 
-alert("Hello ICET");
+// alert("Hello ICET");
 
-let value = "10";
-console.log(typeof value);
+// let value = "10";
+// console.log(typeof value);
 
-// Arithmetic operators
-// + | - | * | /
-let x = 10;
-let y = 20;
-let z = x / y;
+// // Arithmetic operators
+// // + | - | * | /
+// let x = 10;
+// let y = 20;
+// let z = x / y;
 
-console.log(z);
+// console.log(z);
 
-// Comparison operators
-// > | < | >= | <= | == | !=
-// >== | <== | === | !==
-let a = 11;
-let b = "11";
+// // Comparison operators
+// // > | < | >= | <= | == | !=
+// // >== | <== | === | !==
+// let a = 11;
+// let b = "11";
 
-console.log(a === b);
+// console.log(a === b);
 
-// Logical operators
-// && | || | !
-if (!false) {
-    console.log("true");
-} else {
-    console.log("false");
+// // Logical operators
+// // && | || | !
+// if (!false) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+// //if else statement
+// let x = 10;
+// let y = 20;
+
+// if (x > y) {
+//     console.log("x is greater than y");
+// } else if (x < y) {
+//     console.log("x is less than y");
+// } else {
+//     console.log("x is equal to y");
+// }
+
+// // switch statement
+// let day = 2;
+// switch (day) {
+//     case 1: console.log("Monday");
+//         break;
+//     case 2: console.log("Tuesday");
+//         break;
+//     case 3: console.log("Wednesday");
+//         break;
+//     default: console.log("Invalid day");
+// }   
+
+
+//looping statements
+// for loop
+for (let i = 0; i < 5; i++) {
+    console.log(i);
 }
-
-//if else statement
-let x = 10;
-let y = 20;
-
-if (x > y) {
-    console.log("x is greater than y");
-} else if (x < y) {
-    console.log("x is less than y");
-} else {
-    console.log("x is equal to y");
-}
-
-// switch statement
-let day = 2;
-switch (day) {
-    case 1: console.log("Monday");
-        break;
-    case 2: console.log("Tuesday");
-        break;
-    case 3: console.log("Wednesday");
-        break;
-    default: console.log("Invalid day");
-}   
