af7281a5f453850a36a0b9b0d9d57e56bede1fd2
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-06-27 15:05:28 +0530

    While loop

--- Day_05 (2026.06.27)/js/app.js
+++ Day_05 (2026.06.27)/js/app.js
@@ -62,6 +62,13 @@
 
 //looping statements
 // for loop
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// while loop
+ let i = 0;
+ while (i < 5) {
+     console.log(i);
+     i++;
+ }
