cfe92073456dd38d8fc8bf883298410ffea9bec0
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-07-04 11:03:53 +0530

    Dom

--- Day_06 (2026.07.04)/app.js
+++ Day_06 (2026.07.04)/app.js
@@ -21,51 +21,55 @@
 // ---------------------------------------------
 
 //--------2. value eka assign karala object  hadala----------------//
-let customer2 = {
-    name: "John",
-    age: 25,
-    address: "New York",
-    items: [                                 // array eka hadala object ekata assign karala thiyenawa
-        {
-            id: 1,
-            name: 'variant1',
-            price: 100,
-            variants: [
-                {
-                    id: 1,
-                    name: 'variant1',
-                    price: 100,
-                },
-                {
-                    id: 2,
-                    name: 'variant2',
-                }
-            ]
-        },
-        {
-            id: 2,
-            name: 'variant2',
-            price: 200
-        }, {
-            id: 3,
-            name: 'variant3',
-            price: 300
-        }
-    ], 
-    farher: {                           // object ekak hadala object ekata assign karala thiyenawa🚀
-        name: "Peter",
-        age: 50,
-        address: "New York"
-    }
+// let customer2 = {
+//     name: "John",
+//     age: 25,
+//     address: "New York",
+//     items: [                                 // array eka hadala object ekata assign karala thiyenawa
+//         {
+//             id: 1,
+//             name: 'variant1',
+//             price: 100,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name: 'variant1',
+//                     price: 100,
+//                 },
+//                 {
+//                     id: 2,
+//                     name: 'variant2',
+//                 }
+//             ]
+//         },
+//         {
+//             id: 2,
+//             name: 'variant2',
+//             price: 200
+//         }, {
+//             id: 3,
+//             name: 'variant3',
+//             price: 300
+//         }
+//     ], 
+//     farher: {                           // object ekak hadala object ekata assign karala thiyenawa🚀
+//         name: "Peter",
+//         age: 50,
+//         address: "New York"
+//     }
 
-};
-console.log(customer2.name);
-console.log(customer2.age);
-console.log(customer2.address);
-console.log(customer2.items[0].variants[0].name);
-console.log(customer2.farher.name);
-console.log(customer2.farher.age);
-console.log(customer2.farher.address);
+// };
+// console.log(customer2.name);
+// console.log(customer2.age);
+// console.log(customer2.address);
+// console.log(customer2.items[0].variants[0].name);
+// console.log(customer2.farher.name);
+// console.log(customer2.farher.age);
+// console.log(customer2.farher.address);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//---------------------------Dom/ Dom maniqulation-------------------------
+document.write("<h1>Hello World! </h1>");
+
+console.log(document);
