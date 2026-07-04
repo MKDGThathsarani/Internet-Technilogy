d374c7273f9d3101ae75fb83233cf71f3dd1fd41
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-07-04 09:00:08 +0530

    Day 5

--- Day_05 (2026.06.27)/js/app.js
+++ Day_05 (2026.06.27)/js/app.js
@@ -1,41 +1,71 @@
 // let name = "Saman";
+
 // let age = 18;
 
-// name = "Ranil";
+// name = "ranil";
 
 // console.log(name);
 // console.log(age);
 
-// alert("Hello ICET");
+// alert("Hello icet");
 
-// let value = "10";
-// console.log(typeof value);
 
-// // Arithmetic operators
-// // + | - | * | /
+// String name = "10";
+
+// ---------------------------------
+
+// let name = "saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customers = [];
+// let customer = {};
+// let salary = 7500.0;
+
+// console.log(typeof name);
+
+// --------------------------------------------
+//  arithmetic operators
+
+// + | - | * | /
+
 // let x = 10;
 // let y = 20;
+
 // let z = x / y;
 
 // console.log(z);
 
-// // Comparison operators
-// // > | < | >= | <= | == | !=
-// // >== | <== | === | !==
-// let a = 11;
-// let b = "11";
+// -------------------------------------------
 
-// console.log(a === b);
+//  comparison operators
 
-// // Logical operators
-// // && | || | !
+// > | < | >= | <= | == | !=
+// >== | <== | === | !==
+
+
+// let x = 11;
+
+// let y = "11";
+
+// console.log(x===y);
+
+
+// -------------------------------------------
+
+//  logical operators
+
+//  && | || | !
+
 // if (!false) {
 //     console.log("true");
 // } else {
 //     console.log("false");
 // }
 
-// //if else statement
+// -------------------------------------------
+
+// if else statement
+
 // let x = 10;
 // let y = 20;
 
@@ -47,68 +77,90 @@
 //     console.log("x is equal to y");
 // }
 
-// // switch statement
-// let day = 2;
+// -------------------------------------------
+
+// switch statement
+
+// let day = 1;
+
 // switch (day) {
-//     case 1: console.log("Monday");
+//     case 1:
+//         console.log("Monday");
 //         break;
-//     case 2: console.log("Tuesday");
+//     case 2:
+//         console.log("Tuesday");
 //         break;
-//     case 3: console.log("Wednesday");
+//     case 3:
+//         console.log("Wednesday");
 //         break;
-//     default: console.log("Invalid day");
-// }   
+//     default:
+//         console.log("Invalid day");
+// }
 
+// -------------------------------------------
 
-//looping statements
 // for loop
-// for (let i = 0; i < 5; i++) {
+
+// for (let i = 0; i <= 10; i++) {
 //     console.log(i);
 // }
 
+// -------------------------------------------
+
 // while loop
-//  let i = 0;
-//  while (i < 5) {
-//      console.log(i);
-//      i++;
-//  }
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// -------------------------------------------
 
 // do while loop
+
 // let i = 0;
 // do {
 //     console.log(i);
 //     i++;
-// } while (i < 5);            
+// } while (i <= 10);
 
 
+// -------------------------------------------
 
+// function
 
-
-
-
-
-///////////////////////////FUNCTIONS///////////////////////////
-// function add(x,y){
+// function add(x, y) {
 //     return x + y;
 // }
 
-// let result = add(10,20);
+// let result = add(10, 20);
 // console.log(result);
 
-///////////////////////////////ARRAYS///////////////////////////
-// let numbers = [1,2,3,4,5];
+
+// public static String add(int x, int y) {
+//     return x + y;
+// }
+
+// -------------------------------------------
+
+// array
+
+// let numbers = [1, true, 3, "saman", 5 , "ranil","sunil"];
 
 // for (let i = 0; i < numbers.length; i++) {
 //     console.log(numbers[i]);
-// }   
+// }
 
+// -------------------------------------------
 
+// object
 
-/////////////////////////////OBJECTS///////////////////////////////
 let customer = {
-    name: "John",
-    age: 30,
-    email: "john@example.com"
-};
+    name: "Saman",
+    age: 18,
+    isAfterAL: false,
+    salary: 7500.0
+}
 
 console.log(customer);
