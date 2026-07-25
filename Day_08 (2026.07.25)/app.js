f10e0d5fe296830f15df5704919ca65eb4e3010d
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-07-25 15:12:55 +0530

    Refactor HTML structure: update meta tags, add search input and content section for weather data display

--- Day_08 (2026.07.25)/app.js
+++ Day_08 (2026.07.25)/app.js
@@ -1,121 +1,171 @@
-console.log("Hii");
+// console.log("Hii");
 
-// localStorage.clear()
+// // localStorage.clear()
 
-// localStorage.setItem("name","ranil");
-// localStorage.setItem("name","wimal");
-// localStorage.setItem("name1","kamal");
+// // localStorage.setItem("name","ranil");
+// // localStorage.setItem("name","wimal");
+// // localStorage.setItem("name1","kamal");
 
 
-// localStorage.removeItem("name1");
+// // localStorage.removeItem("name1");
 
-// let custName = localStorage.getItem("name");
+// // let custName = localStorage.getItem("name");
 
-// console.log(custName);
-// --------------------------------------------------
+// // console.log(custName);
+// // --------------------------------------------------
 
-// localStorage.clear();
+// // localStorage.clear();
 
-// let customer ={
-//     name:"kamal",
-//     age:12,
-//     isActive:true
+// // let customer ={
+// //     name:"kamal",
+// //     age:12,
+// //     isActive:true
+// // }
+
+// // let stringCustomer = JSON.stringify(customer);
+
+// // localStorage.setItem("customer",stringCustomer);
+
+// // let retCustomer = localStorage.getItem("customer");
+
+// // let jsonCustoemr = JSON.parse(retCustomer);
+
+// // console.log(jsonCustoemr.name);
+
+// // console.log(customer);
+
+// // ---------------------------------------------------
+
+// // const customerList = JSON.parse(localStorage.getItem("customerList"));
+
+// // function btnAddCustomerOnAction() {
+// //     //  let customerList = JSON.parse(localStorage.getItem("customerList"));
+// //     let customer = {
+// //         id: document.getElementById("txtCustomerId").value,
+// //         name: document.getElementById("txtCustomerName").value,
+// //         age: document.getElementById("txtCustomerAge").value,
+// //         address: document.getElementById("txtCustomerAddress").value
+// //     }
+// //     console.log(customer);
+
+// //     customerList.push(customer);
+
+// //     localStorage.setItem("customerList", JSON.stringify(customerList));
+// //     btnLoadTableOnAction();
+
+// //     // alert("add customer")
+// // }
+
+// function btnSearchByIdOnAction() {
+//     let customerList = JSON.parse(localStorage.getItem("customerList"));
+
+//     let customer = customerList.find(customer => {
+//         return customer.id === document.getElementById("txtCustomerId").value
+//     });
+
+
+//     document.getElementById("txtCustomerName").value = customer.name;
+//     document.getElementById("txtCustomerAge").value = customer.age;
+//     document.getElementById("txtCustomerAddress").value = customer.address;
+
+
+//     // alert("search customer")
 // }
 
-// let stringCustomer = JSON.stringify(customer);
+// function btnDeleteByIdOnAction() {
+//     let customerList = JSON.parse(localStorage.getItem("customerList"));
 
-// localStorage.setItem("customer",stringCustomer);
+//     let customerId = document.getElementById("txtCustomerId").value;
 
-// let retCustomer = localStorage.getItem("customer");
+//     let index = customerList.findIndex(customer =>{
+//         return customer.id === customerId;
+//     })
 
-// let jsonCustoemr = JSON.parse(retCustomer);
+//     customerList.splice(index,1);
 
-// console.log(jsonCustoemr.name);
+//     localStorage.setItem("customerList", JSON.stringify(customerList));
 
-// console.log(customer);
-
-// ---------------------------------------------------
-
-const customerList = JSON.parse(localStorage.getItem("customerList"));
-
-function btnAddCustomerOnAction() {
-    //  let customerList = JSON.parse(localStorage.getItem("customerList"));
-    let customer = {
-        id: document.getElementById("txtCustomerId").value,
-        name: document.getElementById("txtCustomerName").value,
-        age: document.getElementById("txtCustomerAge").value,
-        address: document.getElementById("txtCustomerAddress").value
-    }
-    console.log(customer);
-
-    customerList.push(customer);
-
-    localStorage.setItem("customerList", JSON.stringify(customerList));
-    btnLoadTableOnAction();
-
-    // alert("add customer")
-}
-
-function btnSearchByIdOnAction() {
-    let customerList = JSON.parse(localStorage.getItem("customerList"));
-
-    let customer = customerList.find(customer => {
-        return customer.id === document.getElementById("txtCustomerId").value
-    });
+//     console.log(customerList);
+// }
 
 
-    document.getElementById("txtCustomerName").value = customer.name;
-    document.getElementById("txtCustomerAge").value = customer.age;
-    document.getElementById("txtCustomerAddress").value = customer.address;
+// function btnUpdateByIdOnAction() {
+//     alert("update customer");
+// }
 
+// function btnClearStorageOnAction() {
+//     localStorage.clear();
+// }
 
-    // alert("search customer")
-}
+// function btnLoadTableOnAction() {
+//     let customerList = JSON.parse(localStorage.getItem("customerList"));
 
-function btnDeleteByIdOnAction() {
-    let customerList = JSON.parse(localStorage.getItem("customerList"));
+//     let body = `
+//       <tr>
+//             <th>ID</th>
+//             <th>Name</th>
+//             <th>Age</th>
+//             <th>Address</th>
+//         </tr>
+//     `;
+//     customerList.forEach(element => {
+//         body+=`
+//                 <tr>
+//             <td>${element.id}</td>
+//             <td>${element.name}</td>
+//             <td>${element.age}</td>
+//             <td>${element.address}</td>
+//         </tr>
+//         `
+//     });
 
-    let customerId = document.getElementById("txtCustomerId").value;
+//     document.getElementById("tblCustomer").innerHTML=body;
 
-    let index = customerList.findIndex(customer =>{
-        return customer.id === customerId;
-    })
-
-    customerList.splice(index,1);
-
-    localStorage.setItem("customerList", JSON.stringify(customerList));
-
-    console.log(customerList);
-}
-
-
-function btnUpdateByIdOnAction() {
-    alert("update customer");
-}
-
-function btnClearStorageOnAction() {
-    localStorage.clear();
-}
-
-function btnLoadTableOnAction() {
-    let customerList = JSON.parse(localStorage.getItem("customerList"));
-
-    let body = "";
-    customerList.forEach(element => {
-        body+=`
-                <tr>
-            <td>${element.id}</td>
-            <td>${element.name}</td>
-            <td>${element.age}</td>
-            <td>${element.address}</td>
-        </tr>
-        `
-    });
-
-    document.getElementById("tblCustomer").innerHTML=body;
-
-    console.log(body);
+//     console.log(body);
     
 
+// }
+
+
+
+// -------------------------------------------------------------------------------------
+
+const apiKey = "ebd9f33948aa4e169ea85128262507"
+
+const baseUrl = "http://api.weatherapi.com/v1"
+
+fetch(`${baseUrl}/current.json?key=${apiKey}&q=panadura`).then(res=>res.json()).then(data=>{
+    console.log(data);
+
+    document.getElementById("contentSection").innerHTML=`
+                <div>
+                <h1>${data.current.condition.text}</h1>
+                <h1>${data.location.name}</h1>
+                <img src="${data.current.condition.icon}" alt>
+                <p>${data.location.country}</p>
+                <p>${data.current.temp_c}</p>
+            </div>
+    `
+    
+})
+
+function btnSearchOnAction(){
+    let txtUserSearchValue = document.getElementById("txtSearch").value;
+    fetch(`${baseUrl}/current.json?key=${apiKey}&q=${txtUserSearchValue}`).then(res=>res.json()).then(data=>{
+    console.log(data);
+
+    document.getElementById("contentSection").innerHTML=`
+                <div>
+                <h1>${data.current.condition.text}</h1>
+                <h1>${data.location.name}</h1>
+                <img src="${data.current.condition.icon}" alt>
+                <p>${data.location.country}</p>
+                <p>${data.current.temp_c}</p>
+            </div>
+    `
+    
+})
 }
+
+
 
