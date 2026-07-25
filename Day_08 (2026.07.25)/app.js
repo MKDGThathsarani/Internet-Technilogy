fe3f23cb5afdbb9101b80ea4010aed73e636aa93
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-07-25 09:08:06 +0530

    Add initial HTML structure and JavaScript file for Day 08

---
+++ Day_08 (2026.07.25)/app.js
@@ -0,0 +1,25 @@
+console.log("Hi");
+
+//localStorage.clear()
+
+//localStorage.setItem("name","nimal");
+
+//localStorage.setItem("age", 25);
+
+//localStorage.clear();
+//localStorage.clear();
+
+//////////////CREATE/////////////////////
+// localStorage.setItem("name1","Kamal");
+// localStorage.setItem("name2","Nimal");
+// localStorage.setItem("name3","Sunil");
+// localStorage.setItem("name4","pawan");
+
+// //////////////REMOVE/////////////////////
+// localStorage.removeItem("name1");
+
+// //////////////READ/////////////////////
+// let custName = localStorage.getItem("name2");
+// console.log(custName);
+
+///////////////////////////////////////////////////////////////////////////////////
